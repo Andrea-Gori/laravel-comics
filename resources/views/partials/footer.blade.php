9d1465cc7c868885705c305afa5330b72b9fb2cb
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-05-12 23:32:43 +0200

    footer work in progress

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,48 @@
+<footer>
+  <div class="container">
+    <div class="liste">
+      <ul class="comics">
+        <li class="list-title"><a href="">dc comics</a></li>
+        <li><a href="">Characters</a></li>
+        <li><a href="">Comics</a></li>
+        <li><a href="">Movies</a></li>
+        <li><a href="">TV</a></li>
+        <li><a href="">Games</a></li>
+        <li><a href="">Videos</a></li>
+        <li><a href="">News</a></li>
+        <li class="list-title"><a href="">Shop</a></li>
+        <li><a href="">Shop DC</a></li>
+        <li><a href="">Shop DC Collectibles</a></li>
+      </ul>
+
+      <ul class="dc">
+        <li class="list-title"><a href="">DC</a></li>
+        <li><a href="">Terms Of Use</a></li>
+        <li><a href="">Privacy Policy (New)</a></li>
+        <li><a href="">Ad Choices</a></li>
+        <li><a href="">Advertising</a></li>
+        <li><a href="">Jobs</a></li>
+        <li><a href="">Subscriptions</a></li>
+        <li><a href="">Talent Workshops</a></li>
+        <li><a href="">CPSC Certificates</a></li>
+        <li><a href="">Ratings</a></li>
+        <li><a href="">Shop Help</a></li>
+        <li><a href="">Contact Us</a></li>
+      </ul>
+
+      <ul class="sites">
+        <li class="list-title"><a href="">sites</a></li>
+        <li><a href="">DC</a></li>
+        <li><a href="">MAD Magazine</a></li>
+        <li><a href="">DC Kids</a></li>
+        <li><a href="">DC Universe</a></li>
+        <li><a href="">DC Power Visa</a></li>
+      </ul>
+      
+    </div>
+    <p>
+        All Site Content TM and © 2020 DC Entertainment, unless otherwise  <span class="blue"><a href="">noted here</a></span>. All rights reserved.
+        <span class="blue"><a href="">Cookie Settings</a></span>
+    </p>
+  </div>
+</footer>
