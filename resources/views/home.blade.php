4fb22ed38993d981363563b7f1f65bc4f4b192f6
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-05-12 18:27:44 +0200

    header completo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('main')
+  <div class="container">
+    <h2>Current Series</h2>
+
+    
+  </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+  <nav class="nav-top">
+    <div class="container">
+      <span>dc power &#8480 visa &#174 </span>
+      <select name="" id="">
+        <option value="series">additionals dc sites</option>
+      </select>
+      </div>
+  </nav>
+  <nav class="nav-bot">
+    <div class="container">
+      <ul>
+        <li><img src="/images/dc-logo.png" alt=""></li>
+        <li><a href="">Characters</a></li>
+        <li><a href="">comics</a></li>
+        <li><a href="">movies</a></li>
+        <li><a href="">tv</a></li>
+        <li><a href="">games</a></li>
+        <li><a href="">collectibles</a></li>
+        <li><a href="">videos</a></li>
+        <li><a href="">fans</a></li>
+        <li><a href="">news</a></li>
+        <li><a href="">shop<i class="fas fa-caret-down"></i></a></li>
+        <li id="search"><input type="text" placeholder="Search"><i class="fas fa-search"></i></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="jumbo">
+    
+  </div>
+
+
+</header>
